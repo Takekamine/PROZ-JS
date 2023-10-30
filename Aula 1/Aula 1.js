2b65cac207ad3d57d46e3376cd55f01b2fb1737c
Lucas Akamine <142038078+Takekamine@users.noreply.github.com>
2023-10-29 21:58:13 -0300

    tudo

---
+++ Aula 1/Aula 1.js
@@ -0,0 +1,49 @@
+// const num =5; //Constante
+// let num =5; //Variavel
+// || = ou   && = e
+// not = !
+
+// const id =123;
+// let username = 'aluno123'
+// console.log(id, username)
+
+// username = 'aluno456'
+// console.log(id, username)
+
+// let novoId=id
+// console.log(novoId,username)
+
+// novoId= 456
+// console.log(novoId,username)
+
+
+
+// const email = 'xxxx@gmail.com'
+
+// if (email === username || id===123)
+
+// {
+//     console.log('Deseja mudar de senha ?')
+// } else {
+//     console.log('Usuário sem autorização')
+// }
+
+// let idade=25
+// let possuicnh = true
+
+// if(idade>=18 && possuicnh=== true){
+//     console.log('Habilitado')
+// } else {
+//     'Não habilitado'
+// }
+
+
+for(let i =0; i<10;i++){
+    console.log(i);
+}
+
+let letras = ['a','b','c']
+for(let i=0;i<letras.length;i++){
+    console.log(letras[i])
+}
+
